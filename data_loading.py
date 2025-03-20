afb307df5ef9e1b1c737bb29f2c9e54d4eea3c78
PB4490045 <p.b.vandenberg@student.tudelft.nl>
2025-03-20 14:19:54 +0100

    added creation of new json files

--- data_loading.py
+++ data_loading.py
@@ -24,6 +24,8 @@ def load_fold_train_data(folder_path, fold, json_file="split_indices.json"):
     # Find the training indices for the requested fold from the "folds" list.
     fold_train_indices = None
     fold_val_indices = None
+    test_indices = split_indices["test_indices"]
+    
     for fold_entry in split_indices["folds"]:
         if fold_entry["fold"] == fold:
             fold_train_indices = fold_entry["train_indices"]
@@ -42,12 +44,43 @@ def load_fold_train_data(folder_path, fold, json_file="split_indices.json"):
     y_train = y[np.array(fold_train_indices)]
     x_val   = x[np.array(fold_val_indices)]
     y_val   = y[np.array(fold_val_indices)]
+    x_test  = x[np.array(test_indices)]
+    y_test  = y[np.array(test_indices)]
     
-    return x_train, y_train, x_val, y_val
+    return x_train, y_train, x_val, y_val, x_test, y_test, fold_train_indices, fold_val_indices, test_indices
+
+
+def save_set(set_data, output_path, set_name, fold, indices): 
+    """
+    Saves individual numpy arrays with corresponding indices as separate files.
+    
+    Parameters:
+        set_data (numpy array): Data to be saved.
+        output_path (str): Path to the folder where files should be saved.
+        set_name (str): Name of the dataset (e.g., "train").
+        fold (int): Fold number.
+        indices (list): List of indices corresponding to each saved file.
+    """
+    # Ensure the directory exists
+    output_folder = os.path.join(output_path, f"Fold{fold}")
+    os.makedirs(output_folder, exist_ok=True)
+    
+    # Create a subfolder for the dataset
+    set_folder = os.path.join(output_folder, set_name)
+    os.makedirs(set_folder, exist_ok=True)
+    
+    # np.save(os.path.join(output_folder, f"{set_name}.npy"), set)
+    # Save each numpy array individually with its corresponding index
+    for i, index in enumerate(indices):
+        np.save(os.path.join(set_folder, f"{set_name}_{index}.npy"), set_data[i])
 
 if __name__ == "__main__":
     folder_path = r"\\clin-storage\Group Ruers\Students personal folder\Bart van den Berg (M2)_TUDelft_2025\Data\Collection"
+    output_path = r"\\clin-storage\Group Ruers\Students personal folder\Bart van den Berg (M2)_TUDelft_2025\Data\cocoformat"
     fold = 0
-    x_train, y_train = load_fold_train_data(folder_path, fold)
+    x_train, y_train, x_val, y_val, x_test, y_test, fold_train_indices, fold_val_indices, test_indices = load_fold_train_data(folder_path, fold)
     print(f"Loaded training data for fold {fold} with shapes: {x_train.shape}, {y_train.shape}")
+    # save_set(x_train, output_path, "train", fold, fold_train_indices)
+    # save_set(x_val, output_path, "validation", fold, fold_val_indices)
+    save_set(x_test, output_path, "test", fold, test_indices)
 
